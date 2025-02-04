ffe8e5a4c66912adfeec1e93ea42edddd534872b
Helian Guillaume <hguillau@c1r8p3.42nice.fr>
2025-02-04 11:45:11 +0100

    1st push

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,31 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+private:
+    std::string _name;
+    int         _grade;
+
+public:
+    Bureaucrat ( void );
+    Bureaucrat (const Bureaucrat &src);
+    virtual ~Bureaucrat ( void );
+
+//operator
+    Bureaucrat&          operator=(const Bureaucrat &src);
+
+//get & set
+    void setName(std::string Name);
+    void setGrade(int grade);
+    std::string getName( void );
+    int         getGrade( void );
+
+//functions
+    void incrementGrade(/* arguments */);
+    void decrementGrade(/* arguments */);
+};
+
+#endif
