db3c32025e01bd32c676254c35d40443649256e9
Helian Guillaume <hguillau@c1r7p1.42nice.fr>
2025-03-12 15:47:15 +0100

    preparing ex01

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat {
+private:
+    const   std::string _name;
+    int                 _grade;
+
+public:
+    Bureaucrat ( void );
+    Bureaucrat (const std::string &name, int grade);
+    Bureaucrat (Bureaucrat const &src);
+    virtual ~Bureaucrat ( void );
+
+//operator
+    Bureaucrat&         operator=(const Bureaucrat &src);
+
+//getter
+    std::string getName( void ) const;
+    int         getGrade( void ) const;
+
+//functions
+    void incrementGrade();
+    void decrementGrade();
+    class	GradeTooHighException : public std::exception { // < 1
+			public:
+				virtual const char* what() const throw();
+		};
+
+	class	GradeTooLowException : public std::exception { // >150
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
